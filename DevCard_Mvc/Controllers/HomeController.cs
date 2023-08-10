edcc8d9a29b8c74185a1e3dba98f380db7c71161
sahba11 <sahbakavei@gmail.com>
2023-08-10 17:09:16 +0330

    Index&Contact page added

--- DevCard_Mvc/Controllers/HomeController.cs
+++ DevCard_Mvc/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using DevCard_Mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_Mvc.Controllers
 {
@@ -23,11 +19,11 @@ namespace DevCard_Mvc.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
-            return View();
+	        return View();
         }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
