224a9e44732f3f472f81c0b52483534ddd947841
sahba11 <sahbakavei@gmail.com>
2023-08-11 08:22:20 +0330

    Project Section from pages moved to partial view project

---
+++ DevCard_Mvc/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿namespace DevCard_Mvc.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
