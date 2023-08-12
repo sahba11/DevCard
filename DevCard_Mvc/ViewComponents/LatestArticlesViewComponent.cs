f20e8769f77be41c9b581861b8d9dadee775b98a
sahba11 <sahbakavei@gmail.com>
2023-08-12 11:10:16 +0330

    Projects &Latest articles moved to View Component

---
+++ DevCard_Mvc/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DevCard_Mvc.Models;
+
+
+
+namespace DevCard_Mvc.ViewComponents
+{
+	public class LatestArticlesViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+
+			var articles = new List<Article>()
+			{
+				new Article(1,"آموزش Asp.net Core mvc","کاملترین پکیج آموزشasp.net cor mvc","blog-post-thumb-1.jpg"),
+				new Article(2," Git and GitHubآموزش Asp.net Core mvc","کاملترین پکیج آموزشasp.net cor mvc","blog-post-thumb-2.jpg"),
+				new Article(3,"آموزش Onion Architecture","کاملترین پکیج آموزشasp.net cor mvc","blog-post-thumb-3.jpg"),
+				new Article(4,"آموزش طراحی سایت","کاملترین پکیج آموزشasp.net cor mvc","blog-post-thumb-4.jpg")
+
+
+
+			};
+			return View("_latestArticles", articles);
+		}
+
+	}
+}
---
+++ DevCard_Mvc/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections.Generic;
+using DevCard_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Project = DevCard_Mvc.Models.Project;
+
+namespace DevCard_Mvc.ViewComponents
+{
+	public class ProjectsViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var projects = new List<Project>
+			{
+				new Project(1, "تاکسی", "درخواست آنلاین تاکسی برای سفرهای درون شهری","project-1.jpg" ,"Atriya"),
+				new Project(2, "زود فود", "درخواست آنلاین غذا برای سراسر کشور","project-2.jpg", "ZoodFood"),
+				new Project(3, "مدارس", "سیستم مدیریت یک پارچه", "project-3.jpg","Monop"),
+				new Project(4, "فضا پیما", "درخواست آنلاین تاکسی برای سفرهای درون شهری", "project-4.jpg","nasa"),
+			};
+			
+			return View("_Projects",projects);
+		}
+	}
+
+
+}
